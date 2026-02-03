90294267869cfd613129776c85801b1bfc7b31c2
Avi Rai <avirai@umich.edu>
2026-02-02 21:54:24 -0500

    Commit

--- Discussion3.py
+++ Discussion3.py
@@ -3,27 +3,31 @@ import math
 class Rectangle():
     # Create the constructor "__init__" method
 
-    # YOUR CODE HERE
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
 
 
     # Create the "__str__" method
 
-    # YOUR CODE HERE
+    def __str__(self):
+        return str(self.length) + ", " + str(self.width)
 
 
 
     # Create the "area_calculator" method
 
-    # YOUR CODE HERE
-
+    def area_calculator(self):
+        return self.length * self.width
 
 
     # Create the "__eq__" method
     # 
     # Returns a boolean value
 
-    # YOUR CODE HERE
+    def __eq__(self, value):
+        return self.length == value.length and self.width == value.width
 
 
     
@@ -43,10 +47,6 @@ def main():
     # call the __eq__ method
     print(r1 == r2)
     print()
-
-    # you can create additional rectangle objects to 
-    # test your code or learn more about how the class behaves
-    pass
 
 if __name__ == "__main__":
     main()
